2e58f7c0991658e6ae765b5a75c9f246413c9ccd
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2012-12-17 07:07:20 +0100

    Add update script
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>

---
+++ update.sh
@@ -0,0 +1,71 @@
+# Simple cd'er and git pull/push script to ease maintaining multiple forks
+# Place at root of tree
+root=`pwd`
+
+aokp() {
+git pull aokp jb-mr1
+}
+
+cm() {
+git pull cm cm-10.1
+}
+
+push() {
+revision=$1
+git push gh $revision
+}
+
+cd build
+	aokp
+        push jb-mr1
+cd $root
+cd frameworks/av
+	cm
+	push cm-10.1
+cd $root
+cd frameworks/base
+	aokp
+	push jb-mr1
+cd $root
+cd frameworks/native
+	cm
+	push cm-10.1
+cd $root
+cd frameworks/opt/telephony
+	cm
+	push cm-10.1
+cd $root
+cd packages/apps/Camera
+	aokp
+	push jb-mr1
+cd $root
+cd packages/apps/ROMControl
+	aokp
+	push jb-mr1
+cd $root
+cd hardware/libhardware
+	cm
+	push cm-10.1
+cd $root
+cd hardware/qcom/audio
+	cm
+	push cm-10.1
+cd $root
+cd hardware/qcom/display
+	cm
+	push cm-10.1
+cd $root
+cd kernel/samsung/d2
+	cm
+	push jb-mr1
+cd $root
+cd vendor/aokp
+	aokp
+	push jb-mr1
+cd $root
+cd vendor/samsung
+	aokp
+	push jb-mr1
+cd $root
+echo "All synced up"
+exit
