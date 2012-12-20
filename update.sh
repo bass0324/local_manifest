2b26bab2a88a2c4a022b8f066a056e984f420cad
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2012-12-20 17:19:42 -0500

    updater: readd kernel

--- update.sh
+++ update.sh
@@ -6,10 +6,19 @@ aokp() {
 git pull aokp jb-mr1
 }
 
+cm() {
+git pull cm cm-10.1
+}
+
 push() {
 revision=$1
 git push gh $revision
 }
+
+cd kernel/samsung/d2
+    cm
+    push jb-mr1
+cd $root
 
 ## Devices will go here once merged
 echo "All synced up"
