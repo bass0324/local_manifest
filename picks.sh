926572c4fc4f6fcdd6b574a0fcf3d9f49fdae4ec
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2012-12-21 21:05:22 +0100

    local: Add picks script
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>

---
+++ picks.sh
@@ -0,0 +1,23 @@
+# symlinked from local_manifest repo
+# runs pick script from external platform_manifest repo from UnicornButter
+
+root=`pwd`
+
+#Pull in linaro changes on gerrit
+./platform_manifest/pick.sh
+
+cd ~/
+
+#Pull in non-device cherries
+cd frameworks/base
+git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/24/4724/3 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/48/4748/1 && git cherry-pick FETCH_HEAD
+cd $root
+cd frameworks/opt/telephony
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/37/28237/1 && git cherry-pick FETCH_HEAD
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/46/28246/4 && git cherry-pick FETCH_HEAD
+cd $root
+cd packages/apps/ROMControl
+git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/25/4725/1 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/52/4752/2 && git cherry-pick FETCH_HEAD
+cd $root
