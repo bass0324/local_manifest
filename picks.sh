a93e9c9da00b5bc4d177909aa63b5e696e36897d
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2012-12-24 00:26:34 -0500

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -16,10 +16,6 @@ cd $root
 cd frameworks/base
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/24/4724/3 && git cherry-pick FETCH_HEAD
 cd $root
-cd frameworks/opt/telephony
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/37/28237/1 && git cherry-pick FETCH_HEAD
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/46/28246/4 && git cherry-pick FETCH_HEAD
-cd $root
 cd packages/apps/ROMControl
 git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/25/4725/1 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/52/4752/2 && git cherry-pick FETCH_HEAD
