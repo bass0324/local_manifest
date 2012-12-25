e1a9cd536aa5637bd54ab886c1e5d8fe009d8740
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2012-12-25 00:12:07 -0500

    add cd to $root back

--- picks.sh
+++ picks.sh
@@ -15,6 +15,7 @@ cd $root
 cd frameworks/base
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/24/4724/3 && git cherry-pick FETCH_HEAD
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/60/28960/3 && git cherry-pick FETCH_HEAD
+cd $root
 cd packages/apps/ROMControl
 git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/25/4725/1 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/52/4752/2 && git cherry-pick FETCH_HEAD
