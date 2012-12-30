70a872aa6b73eb6c675d7efd31debe5e4db05b3b
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2012-12-30 00:09:03 -0500

    add 4869

--- picks.sh
+++ picks.sh
@@ -1,21 +1,23 @@
 # symlinked from local_manifest repo
+# Run from root of source tree
 # runs pick script from external platform_manifest repo from UnicornButter
 
 root=`pwd`
+cd $root
 
-#Pull in linaro changes on gerrit
+# Pull in linaro changes on gerrit
+# if using the pub manifest
 if [ -e platform_manifest/pick.sh ]; then
   cd platform_manifest
   ./pick.sh
 fi
-
-cd $root
 
 #Pull in non-device cherries
 cd frameworks/av
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_av refs/changes/34/4834/1 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_av refs/changes/35/4835/1 && git cherry-pick FETCH_HEAD
 cd $root
+
 cd frameworks/base
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/24/4724/3 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/38/4838/1 && git cherry-pick FETCH_HEAD
@@ -23,16 +25,20 @@ git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/79/477
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/59/4759/2 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/64/4864/4 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/68/4868/1 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/69/4869/1 && git cherry-pick FETCH_HEAD
 cd $root
+
 cd frameworks/opt/telephony
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/00/29100/1 && git cherry-pick FETCH_HEAD
 cd $root
+
 cd packages/apps/ROMControl
 git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/25/4725/1 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/60/4760/3 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/75/4775/2 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/87/4787/1 && git cherry-pick FETCH_HEAD
 cd $root
+
 cd packages/apps/Settings
 git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_Settings refs/changes/65/4865/1 && git cherry-pick FETCH_HEAD
 cd $root
