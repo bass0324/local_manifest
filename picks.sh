fd87a96f67a422714c8b71bce4c74bab92712455
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2013-01-05 04:55:05 -0500

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -18,8 +18,6 @@ cd frameworks/base
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/00/28400/2 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/24/4724/3 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/64/4864/4 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/95/4895/1 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/26/4926/1 && git cherry-pick FETCH_HEAD
 cd $root
 
 cd frameworks/opt/telephony
@@ -32,7 +30,7 @@ git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_Mms refs/changes/59/4
 cd $root
 
 cd packages/apps/ROMControl
-git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/25/4725/1 && git cherry-pick FETCH_HEAD
+#git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/25/4725/1 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/75/4775/2 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/54/4754/11 && git cherry-pick FETCH_HEAD
 cd $root
