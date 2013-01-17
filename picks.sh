b2bfa2c20f36339672e57f1655af2781506b2c36
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2013-01-17 10:07:17 -0500

    PICK ALL TEH THINGZ

--- picks.sh
+++ picks.sh
@@ -15,17 +15,25 @@ fi
 #Pull in non-device cherries
 
 cd frameworks/base
+git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/39/5039/3 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/97/4997/1 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/40/5040/4 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/64/4864/4 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/53/4953/1 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/79/5079/5 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/06/5106/4 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/30/5130/4 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/16/5116/1 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/23/5123/1 && git cherry-pick FETCH_HEAD
 #git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/54/5054/1 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/79/5079/2 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/24/4924/7 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/06/5106/1 && git cherry-pick FETCH_HEAD
 cd $root
 
 cd hardware/qcom/audio-caf
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_qcom_audio-caf refs/changes/13/30213/1 && git cherry-pick FETCH_HEAD
+cd $root
+
+cd packages/apps/Contacts
+git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_Contacts refs/changes/25/5125/4 && git cherry-pick FETCH_HEAD
 cd $root
 
 cd packages/apps/Mms
@@ -39,15 +47,14 @@ cd $root
 cd packages/apps/ROMControl
 git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/54/4954/2 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/80/5080/2 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/23/4923/3 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/07/5107/1 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/07/5107/2 && git cherry-pick FETCH_HEAD
 cd $root
 
 cd packages/apps/Settings
 git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_Settings refs/changes/65/4865/1 && git cherry-pick FETCH_HEAD
 cd $root
 
-cd packages/inputmethods/LatinIME
-git fetch http://gerrit.sudoservers.com/AOKP/packages_input_LatinIME refs/changes/92/4992/1 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/packages_input_LatinIME refs/changes/93/4993/1 && git cherry-pick FETCH_HEAD
+cd vendor/aokp
+git fetch http://gerrit.sudoservers.com/AOKP/vendor_aokp refs/changes/18/5118/2 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/vendor_aokp refs/changes/72/4972/3 && git cherry-pick FETCH_HEAD
 cd $root
