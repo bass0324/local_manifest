7d33bc3bf9e905e0a7715c1e5dda5cf33f322d90
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2013-03-22 05:25:52 -0300

    add 4.8 fix if using gcc

--- picks.sh
+++ picks.sh
@@ -10,6 +10,8 @@ cd $root
 if [ -e platform_manifest/pick.sh ]; then
   cd platform_manifest
   ./pick.sh
+  cd system/core
+  git fetch ssh://BMc@gerrit.sudoservers.com:29418/AOKP/system_core refs/changes/61/7361/1 && git cherry-pick FETCH_HEAD
   cd $root
 fi
 
