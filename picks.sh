1571d252b7c6188946b966b1b4faa905bc675bbe
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2013-03-25 08:23:20 +0100

    Clean up

--- picks.sh
+++ picks.sh
@@ -20,13 +20,3 @@ cd frameworks/base
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/45/7245/5 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/07/7207/6 && git cherry-pick FETCH_HEAD
 cd $root
-
-cd packages/apps/Mms
-git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_Mms refs/changes/59/7259/2 && git cherry-pick FETCH_HEAD
-cd $root
-
-cd vendor/samsung
-git fetch http://gerrit.sudoservers.com/AOKP/vendor_samsung refs/changes/31/7331/2 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/vendor_samsung refs/changes/32/7332/2 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/vendor_samsung refs/changes/58/7358/1 && git cherry-pick FETCH_HEAD
-cd $root
