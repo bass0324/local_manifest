fc89d3d0c183a653b3114c7f87db657d1c10340e
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2013-03-30 01:50:42 -0300

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -20,6 +20,7 @@ cd frameworks/base
 #git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/45/7245/7 && git cherry-pick FETCH_HEAD
 #git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/07/7207/8 && git cherry-pick FETCH_HEAD
 git fetch ssh://BMc@gerrit.sudoservers.com:29418/AOKP/frameworks_base refs/changes/36/7436/3 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/48/7548/1 && git cherry-pick FETCH_HEAD
 cd $root
 
 cd external/webkit
@@ -29,4 +30,5 @@ cd $root
 
 cd packages/apps/ROMControl
 git fetch ssh://BMc@gerrit.sudoservers.com:29418/AOKP/packages_apps_ROMControl refs/changes/35/7435/3 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/49/7549/1 && git cherry-pick FETCH_HEAD
 cd $root
