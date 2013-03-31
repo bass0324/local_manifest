52821f20b962a52bc4bc7134d1086639fa3beb27
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2013-03-31 01:28:17 -0300

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -28,10 +28,10 @@ git fetch http://gerrit.sudoservers.com/AOKP/device_samsung_d2vzw refs/changes/8
 cd $root
 
 cd frameworks/base
-#git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/45/7245/7 && git cherry-pick FETCH_HEAD
-#git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/07/7207/8 && git cherry-pick FETCH_HEAD
 git fetch ssh://BMc@gerrit.sudoservers.com:29418/AOKP/frameworks_base refs/changes/36/7436/4 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/48/7548/1 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/45/7245/8 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/07/7207/9 && git cherry-pick FETCH_HEAD
 cd $root
 
 cd external/webkit
@@ -42,4 +42,5 @@ cd $root
 cd packages/apps/ROMControl
 git fetch ssh://BMc@gerrit.sudoservers.com:29418/AOKP/packages_apps_ROMControl refs/changes/35/7435/5 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/49/7549/1 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/77/7577/1 && git cherry-pick FETCH_HEAD
 cd $root
