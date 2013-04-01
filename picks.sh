359245b138697cce06656a4a2d45ecf0a3c2947d
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2013-04-01 00:40:36 -0300

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -16,31 +16,12 @@ if [ -e platform_manifest/pick.sh ]; then
   cd $root
 fi
 
-#defconfig merger
-cd device/samsung/d2att
-git fetch http://gerrit.sudoservers.com/AOKP/device_samsung_d2att refs/changes/84/7584/1 && git cherry-pick FETCH_HEAD
-cd ../d2tmo
-git fetch http://gerrit.sudoservers.com/AOKP/device_samsung_d2tmo refs/changes/85/7585/1 && git cherry-pick FETCH_HEAD
-cd ../d2usc
-git fetch http://gerrit.sudoservers.com/AOKP/device_samsung_d2usc refs/changes/83/7583/1 && git cherry-pick FETCH_HEAD
-cd ../d2vzw
-git fetch http://gerrit.sudoservers.com/AOKP/device_samsung_d2vzw refs/changes/82/7582/1 && git cherry-pick FETCH_HEAD
-cd $root
-
 cd frameworks/base
 git fetch ssh://BMc@gerrit.sudoservers.com:29418/AOKP/frameworks_base refs/changes/36/7436/4 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/48/7548/1 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/45/7245/10 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/07/7207/10 && git cherry-pick FETCH_HEAD
-cd $root
-
-cd external/webkit
-git fetch http://gerrit.sudoservers.com/AOKP/external_webkit refs/changes/80/7480/1 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/external_webkit refs/changes/81/7481/1 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/33/5133/1 && git cherry-pick FETCH_HEAD
 cd $root
 
 cd packages/apps/ROMControl
 git fetch ssh://BMc@gerrit.sudoservers.com:29418/AOKP/packages_apps_ROMControl refs/changes/35/7435/5 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/49/7549/1 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/77/7577/1 && git cherry-pick FETCH_HEAD
+git fetch http://gerrit.sudoservers.com/AOKP/packages_apps_ROMControl refs/changes/54/4954/3 && git cherry-pick FETCH_HEAD
 cd $root
