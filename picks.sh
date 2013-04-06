e7f1ebda926a18f82a767bed8bf5e93ebf819496
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2013-04-06 01:23:51 -0300

    remove pub calls

--- picks.sh
+++ picks.sh
@@ -1,23 +1,11 @@
 # symlinked from local_manifest repo
 # Run from root of source tree
-# runs pick script from external platform_manifest repo from UnicornButter
 
 root=`pwd`
 cd $root
 
 # In order to use pstest must be in build environment
 . build/envsetup.sh >/dev/null 2>&1
-
-# Pull in linaro changes on gerrit
-# if using the pub manifest
-if [ -e platform_manifest/pick.sh ]; then
-  cd platform_manifest
-  ./pick.sh
-  cd $root
-  cd system/core
-  pstest 7361/1
-  cd $root
-fi
 
 cd build
 pstest 7717/1
