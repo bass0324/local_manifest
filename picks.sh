82dd31c5397a050144bb3e7617646162ca978047
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2013-04-07 05:14:45 -0300

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -13,8 +13,14 @@ cd $root
 
 cd frameworks/base
 #pstest 7436/
+pstest 7732/1
+pstest 7744/1
+
 cd $root
 
 cd packages/apps/ROMControl
 #pstest 7435/
+pstest 7718/3
+pstest 7742/1
+pstest 7746/1
 cd $root
