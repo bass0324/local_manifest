a62a5e3551aff8d41edd55e73743e0dc4fada87e
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2013-04-25 03:39:55 -0300

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -16,5 +16,11 @@ pstest 7889/10
 pstest 7969/2
 cd $root
 
+cd vendor/samsung
+pstest 7991/1
+pstest 7992/1
+pstest 7993/1
+cd $root
+
 # Reinitialize build environment with pspush
 . build/envsetup.sh >/dev/null 2>&1
