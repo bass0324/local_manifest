f31698c8d5d458e36d5c26010c08fa8dea93a59c
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2013-04-29 02:11:43 -0300

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -12,8 +12,23 @@ pstest 7717/1
 cd $root
 
 cd frameworks/base
-pstest 7969/3
-pstest 8014/2
+pstest 7982/1
+pstest 8032/1
+pstest 8029/2
+pstest 8034/1
+pstest 7969/6
+pstest 8014/4
+pstest 8041/3
+cd $root
+
+cd packages/apps/Camera
+pstest 8048/6
+pstest 8049/5
+pstest 8050/4
+cd $root
+
+cd packages/apps/Settings
+pstest 8035/2
 cd $root
 
 # Reinitialize build environment with pspush
