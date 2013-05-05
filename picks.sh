78dd6042ddd0bb479773336114b43a80c68afc22
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2013-05-05 16:24:54 -0300

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -9,6 +9,26 @@ cd $root
 
 cd build
 pstest 7717/2
+pstest 8192/1
+cd $root
+
+cd device/samsung/d2-common
+pstest 8179/1
+pstest 8180/1
+pstest 8181/1
+pstest 8182/1
+pstest 8183/1
+pstest 8184/1
+pstest 8185/1
+cd $root
+
+cd device/samsung/msm8960-common
+pstest 8169/1
+pstest 8170/1
+pstest 8171/1
+pstest 8172/1
+pstest 8173/1
+pstest 8174/1
 cd $root
 
 cd frameworks/base
@@ -20,6 +40,21 @@ cd $root
 
 cd packages/apps/ROMControl
 pstest 4550/3
+cd $root
+
+cd system/core
+pstest 8193/1
+cd $root
+
+cd vendor/samsung
+pstest 8187/1
+pstest 8188/1
+pstest 8189/1
+pstest 8190/1
+cd $root
+
+cd vendor/aokp
+pstest 8191/1
 cd $root
 
 # Reinitialize build environment with pspush
