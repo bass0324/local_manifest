a2de11ae9b503a7ce01376cd3816685413e5c544
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2013-05-06 12:36:43 -0300

    Create jfltesetup.sh

---
+++ jfltesetup.sh
@@ -0,0 +1,26 @@
+# Run from root of source tree
+
+cd ..
+root=`pwd`
+cd $root
+
+# In order to use pstest must be in build environment
+. build/envsetup.sh >/dev/null 2>&1
+
+cd build
+pstest 8192/1
+cd $root
+
+cd system/core
+pstest 8193/1
+cd $root
+
+cd vendor/samsung
+pstest 8188/1
+pstest 8189/1
+pstest 8190/1
+cd $root
+
+cd vendor/aokp
+pstest 8191/2
+cd $root
