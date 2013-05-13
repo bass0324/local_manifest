ded901039096185e3305792674509819b5d0e854
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2013-05-13 13:58:48 -0300

    Update jfltesetup.sh

--- jfltesetup.sh
+++ jfltesetup.sh
@@ -7,6 +7,10 @@ cd $root
 # In order to use pstest must be in build environment
 . build/envsetup.sh >/dev/null 2>&1
 
+cd frameworks/opt/telephony
+git fetch ssh://BMc@review.cyanogenmod.org:29418/CyanogenMod/android_frameworks_opt_telephony refs/changes/75/37075/2 && git cherry-pick FETCH_HEAD
+cd $root
+
 cd vendor/samsung
 pstest 8188/1
 pstest 8189/1
