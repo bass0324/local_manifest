1dc91b53dfee30483fd4844a7f5d2b0686707a1d
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2013-05-13 13:54:51 -0300

    Separate pick scripts

--- picks.sh
+++ picks.sh
@@ -9,34 +9,15 @@ cd $root
 
 cd build
 pstest 7717/2
-pstest 8192/1
 cd $root
 
 cd frameworks/base
-pstest 7969/6
-pstest 8014/4
 pstest 8126/2
 pstest 8134/1
-pstest 8237/3
 cd $root
 
 cd packages/apps/ROMControl
 pstest 4550/3
-pstest 8236/2
-cd $root
-
-cd system/core
-pstest 8193/1
-cd $root
-
-cd vendor/samsung
-pstest 8188/1
-pstest 8189/1
-pstest 8208/1
-cd $root
-
-cd vendor/aokp
-pstest 8191/2
 cd $root
 
 # Reinitialize build environment with pspush
