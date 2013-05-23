b6ac0a7d6b0857fd665c44ed8f1c2204d36f5cd9
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-05-23 15:37:22 -0300

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -12,12 +12,12 @@ pstest 7717/2
 cd $root
 
 cd frameworks/base
-pstest 8126/2
-pstest 8134/1
+
+
 cd $root
 
 cd packages/apps/ROMControl
-pstest 4550/3
+
 cd $root
 
 # Reinitialize build environment with pspush
