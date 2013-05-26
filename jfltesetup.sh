ce4e5e1e0040e6909afb17ff89c24bbdc40f3df6
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-05-25 20:58:15 -0400

    Update jfltesetup.sh

--- jfltesetup.sh
+++ jfltesetup.sh
@@ -6,14 +6,32 @@ cd $root
 # In order to use pstest must be in build environment
 . build/envsetup.sh >/dev/null 2>&1
 
+cd build
+pstest 7717/2
+pstest 8557/9
+cd $root
+
+cd device/samsung/jf-common
+pstest 8577/1
+cd $root
+
+cd device/samsung/msm8960-common
+pstest 8579/1
+patest 8580/1
+cd $root
+
 cd frameworks/opt/telephony
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/36/37936/31 && git cherry-pick FETCH_HEAD
 cd $root
 
 cd vendor/samsung
 pstest 8208/1
+pstest 8519/4
+pstest 8566/1
 cd $root
 
 cd vendor/aokp
 pstest 8191/2
+pstest 8521/5
+pstest 8567/1
 cd $root
