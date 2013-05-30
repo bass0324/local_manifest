b721f959e624909fc28f3cadee33d82d41e65889
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-05-30 01:12:15 -0300

    Update jfltesetup.sh

--- jfltesetup.sh
+++ jfltesetup.sh
@@ -8,32 +8,27 @@ cd $root
 
 cd build
 pstest 7717/2
-pstest 8557/9
 cd $root
 
 cd device/samsung/jf-common
-pstest 8577/1
+pstest 8589/5
 cd $root
 
-cd device/samsung/msm8960-common
-pstest 8579/1
-patest 8580/1
-cd $root
 
 cd frameworks/opt/telephony
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/36/37936/31 && git cherry-pick FETCH_HEAD
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/36/37936/32 && git cherry-pick FETCH_HEAD
 cd $root
 
 cd vendor/samsung
-pstest 8208/1
-pstest 8519/4
-pstest 8566/1
+pstest 8208/3
+pstest 8519/5
+pstest 8566/2
+pstest 8641/1
 cd $root
 
 cd vendor/aokp
-pstest 8191/2
+pstest 8191/6
 pstest 8521/5
-pstest 8567/1
 cd $root
 
 . build/envsetup.sh >/dev/null 2>&1
