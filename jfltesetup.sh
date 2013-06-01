619620799cfd6538d4f28cc865ac20da39e593bf
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-06-01 23:30:41 +0200

    update picks for jf

--- jfltesetup.sh
+++ jfltesetup.sh
@@ -10,6 +10,18 @@ cd build
 pstest 7717/2
 cd $root
 
+cd device/samsung/jf-common
+pstest 8742/1
+pstest 8743/1
+pstest 8744/1
+pstest 8745/1
+pstest 8746/1
+cd $root
+
+cd packages/apps/Camera
+pstest 8685/1
+cd $root
+
 cd vendor/aokp
 pstest 8191/6
 pstest 8583/1
