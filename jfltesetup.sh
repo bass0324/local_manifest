0070dbdf7c8d0d5fce47f94373166acec56c0c8d
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-06-01 22:51:46 -0300

    thanks protekk

--- jfltesetup.sh
+++ jfltesetup.sh
@@ -10,14 +10,6 @@ cd build
 pstest 7717/2
 cd $root
 
-cd device/samsung/jf-common
-pstest 8742/1
-pstest 8743/1
-pstest 8744/1
-pstest 8745/1
-pstest 8746/1
-cd $root
-
 cd packages/apps/Camera
 pstest 8685/1
 cd $root
